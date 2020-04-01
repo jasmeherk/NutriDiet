b9675e78ab1e414bc488f891cf6730072cea93ce
Jasmeher <jasmeherk@gmail.com>
2020-04-01 09:04:14 -0700

    Phase 4 : Final Project Commit

--- src/main/exceptions/InvalidInputException.java
+++ src/main/exceptions/InvalidInputException.java
@@ -1,6 +1,5 @@
 package exceptions;
 
 public class InvalidInputException extends NumberFormatException {
-    // File not found exception : checked exception
-    // Incorrect data type entered : unchecked exception
+
 }
